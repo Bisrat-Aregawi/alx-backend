ba8d63dc5f3543bb4eae7d78ce847891bdadcee8
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-04 17:15:41 +0300

    Implement `LIFOCache` class inheriting `BaseCaching`
    Override `put` and `get` methods of parent.
    put -> Use LIFO to remove newer entries if MAX_ITEMS is exceeded
        -> Any update to entries should put them on top of the stask (unlike
           FIFO where oldest entry is of concern)
    get -> Retrieve value using key, None if key doesn't exist
    
    For task 2 of Solo-Project: - `0x01-caching`

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Module defines `FIFOCache` class
+"""
+from typing import Any
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class
+
+    Attributes:
+        cache_data: a dictionary representation of in-memory cache
+        MAX_ITEMS: an integer representing size of cache_data
+    """
+    def __init__(self):
+        """Initialize"""
+        super(LIFOCache, self).__init__()
+
+    def put(self, key: Any, item: Any) -> None:
+        """Insert cache entry (key: value) to internal dictionary
+
+        If MAX_ITEMS is passed make sure LIFO is enforced
+        """
+        if key and item is not None:
+            try:
+                if len(self.cache_data) < self.__class__.MAX_ITEMS:
+                    self.cache_data[key] = item
+                elif self.cache_data.get(key) is not None:
+                    self.cache_data.pop(key)
+                    self.cache_data[key] = item
+                else:
+                    raise Exception
+            except Exception:
+                first_in_key = list(self.cache_data.items())[-1][0]
+                self.cache_data.pop(
+                        first_in_key
+                        )
+                print("DISCARD: {}".format(first_in_key))
+                self.cache_data[key] = item
+
+    def get(self, key) -> Any:
+        """Retrieve value using key"""
+        return self.cache_data.get(key)
+    pass
