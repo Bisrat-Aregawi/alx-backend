3c24eb07d20fcec9b7fd4b81e44a4261c9306aac
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-04 18:18:47 +0300

    Implement `LRUCache` class inheriting `BaseCaching`
    Override `put` and `get` methods of parent
    put -> Use LRU to remove old entries if MAX_ITEMS is exceeded
        -> Any update to entries should put them on top of the stack (like
        LIFO)
    get -> Retrieve value using key. Upon call of this method called entry
    should be on top of stack
    
    For task 3 of Solo-Project: - `0x01-caching`

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+""" Module defines LRUCache class
+"""
+from base_caching import BaseCaching
+from typing import Any
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class.
+
+    Attributes:
+        cache_data: a dictionary representation of in-memory cache
+        MAX_ITEMS: an integer representing size of cache_data
+    """
+    def __init__(self):
+        """Initialize."""
+        super(LRUCache, self).__init__()
+
+    def put(self, key: Any, item: Any) -> None:
+        """Insert cache entry(key: value) to internal dictinary
+
+        If MAX_ITEMS is exceeded, LRU is envorced
+        """
+        if key and item is not None:
+            try:
+                if len(self.cache_data) < self.__class__.MAX_ITEMS:
+                    self.cache_data[key] = item
+                elif self.cache_data.get(key) is not None:
+                    self.cache_data.pop(key)
+                    self.cache_data[key] = item
+                else:
+                    raise Exception
+            except Exception:
+                first_in_key = list(self.cache_data.items())[0][0]
+                self.cache_data.pop(
+                        first_in_key
+                        )
+                print("DISCARD: {}".format(first_in_key))
+                self.cache_data[key] = item
+
+    def get(self, key: Any) -> Any:
+        """Retrieve value using key.
+
+        On every call of this method reorient entry to top
+        """
+        val = self.cache_data.get(key)
+        if val:
+            self.cache_data.pop(key)
+            self.cache_data[key] = val
+        return val
