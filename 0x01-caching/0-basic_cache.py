b20cf2a9b3677e1c98575791361f5b0fa790e39f
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-04 12:25:02 +0300

    Fix code according to mypy annotation recommendation
    For task 0 of Solo-Project: - `0x01-caching`

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,17 +2,25 @@
 """ Module defines `BasicCache` class
 """
 from base_caching import BaseCaching
+from typing import Any
 
 
 class BasicCache(BaseCaching):
-    """docstring for BasicCache."""
+    """BasicCache class
+
+    Attributes:
+        cache_data: a dictionary representation of in-memory cache
+    """
     def __init__(self):
         """Initialize"""
         super(BasicCache, self).__init__()
 
-    def put(self, key, item):
+    def put(self, key: Any, item: Any) -> None:
+        """ Insert cache entry (key: value) to internal dictionary
+        """
         if key and item is not None:
             self.cache_data[key] = item
 
-    def get(self, key):
+    def get(self, key: Any) -> Any:
+        """ Retrieve value using key"""
         return self.cache_data.get(key)
