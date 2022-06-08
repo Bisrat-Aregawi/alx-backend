468ab375755bde89edf69977b35e09a5d47bee89
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-06-08 17:22:33 +0300

    Write test for job creation
    For task 11 of Solo-Project: - `0x03-queuing_system_in_js`

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -15,13 +15,15 @@ export default function createPushNotificationsJobs(jobs, queue) {
     throw new Error('Jobs is not an array');
   }
   jobs.forEach((job) => {
-    const newJob = queue.create('notification', job)
-      .save((err) => {
-        // Log error message if job can't be saved
-        if (err) console.error(`Notification job ${newJob.id} failed: ${err}`);
-        // Log message if job save was successful
-        else console.log(`Notification job created: ${newJob.id}`);
-      })
+    const newJob = queue.create('notification', job);
+    newJob.save((err) => {
+      // Log error message if job can't be saved
+      if (err) console.error(`Notification job ${newJob.id} failed: ${err}`);
+    });
+    newJob.on('enqueue', () => {
+      // Log message if job save was successful
+      console.log(`Notification job created: ${newJob.id}`);
+    })
       // Log message when job has completed
       .on('complete', () => {
         console.log(`Notificationn job #${newJob.id} completed`);
